52ffdcd82279c7e82d8ea156ff8a4bee77177ca8
Magesh Kuppan <tkmagesh77@gmail.com>
2019-11-05 11:19:20 +0530

    fp assignment

---
+++ 01-Basics/08-products.js
@@ -0,0 +1,15 @@
+var products = [
+    {id : 7, name : 'Pen', cost : 40, units : 30, category : 'stationary'},
+    {id : 2, name : 'Ten', cost : 70, units : 20, category : 'grocery'},
+    {id : 9, name : 'Den', cost : 20, units : 40, category : 'stationary'},
+    {id : 4, name : 'Len', cost : 60, units : 50, category : 'stationary'},
+    {id : 5, name : 'Zen', cost : 50, units : 30, category : 'grocery'}
+]
+
+Find the following using the 'filter, map & reduce' functions
+
+1. filter stationary products
+2. filter cost products (cost > 50)
+3. filter affordable products
+4. find the total product value (sum of cost * units)
+5. create a new array with the product cost applied with 10% discount
