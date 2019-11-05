7e9d7208b522426e7b00f6a5692f75eb22e3bfdd
Magesh Kuppan <tkmagesh77@gmail.com>
2019-11-05 16:43:21 +0530

    isPrime

---
+++ 01-Basics/11-isPrime.js
@@ -0,0 +1,38 @@
+function add(x,y){
+    return x + y;
+}
+
+add(10,20) => 30
+add(100,200) => 300
+
+var z = 100;
+function add(x,y){
+    return x + y + z;
+}
+
+add(10,20) => 130
+z = 1000;
+add(10,20) => 1030
+
+
+
+
+var isPrime = (function(){
+    function checkPrime(n){
+        console.log('processing ', n);
+        for(var i=2; i <= (n/2); i++)
+            if (n % i === 0)
+                return false;
+        return true;
+    }
+    var cache = {};
+
+    function isPrime(n){
+        if (typeof cache[n] === 'undefined')
+            cache[n] = checkPrime(n);
+        console.log(cache);
+        return cache[n];
+    }
+
+    return isPrime;
+})();
