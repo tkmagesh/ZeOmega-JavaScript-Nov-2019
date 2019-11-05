715b848cd06d0f142ddc2b8f60861d71cbc32a04
Magesh Kuppan <tkmagesh77@gmail.com>
2019-11-05 15:50:50 +0530

    closures demo

---
+++ 01-Basics/10-spinner.js
@@ -0,0 +1,30 @@
+var spinner = /*....*/;
+//up, down
+spinner.up() //=> 1
+spinner.up() //=> 2
+spinner.up() //=> 3
+spinner.up() //=> 4
+
+spinner.down() //=> 3
+spinner.down() //=> 2
+spinner.down() //=> 1
+spinner.down() //=> 0
+spinner.down() //=> -1
+
+spinner.counter = 10000
+counter = 10000
+spinner.up()
+
+var spinner = (function (){
+    var counter = 0;
+    function up(){
+        return ++counter;
+    }
+    function down(){
+        return --counter;
+    }
+    return {
+        up : up,
+        down : down
+    }
+})();
